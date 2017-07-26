5de67a3fbee6a1b74064ecd17540297ecee1e11b
liumengchen@ibantang.com <fbsender@lmc.local>
2017-07-26 23:51:46 +0800

    footer links

--- app/models/iquan.rb
+++ app/models/iquan.rb
@@ -2,6 +2,6 @@ class ProductClick < ApplicationRecord
   self.table_name = 'iquan_product_clicks'
 end
 
-class ICookie < ApplicationRecord
-  self.table_name = 'iquan_cookies'
+class Link < ApplicationRecord
+  self.table_name = 'shikuai_links'
 end
