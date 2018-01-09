3fa9f0899a8b601d9f9c30361f195f83dac72d0e
fbsender <lmctrello@gmail.com>
2018-01-09 11:42:35 +0800

    add spider

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,7 @@ class ApplicationController < ActionController::Base
 
   def is_robot?
     user_agent = request.headers["HTTP_USER_AGENT"]
-    user_agent.present? && user_agent =~ /(bot|spider|slurp)/i
+    user_agent.present? && user_agent =~ /(bot|spider|slurp|Linux x86_64)/i
   end
 
   def is_device_mobile?
